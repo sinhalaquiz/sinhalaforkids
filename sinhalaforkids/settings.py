bb95fa885fc8f2eae35a1620b6ada8fb74046aeb
Dushara Jayasinghe <nidujay@gmail.com>
2015-06-28 08:33:39 +1000

    Renamed site/project to sinhalaforkids

--- sinhalaforkids/settings.py
+++ sinhalaforkids/settings.py
@@ -1,4 +1,4 @@
-# Django settings for gurugeya project.
+# Django settings for sinhalaforkids project.
 import os
 
 DEBUG = True
@@ -107,10 +107,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'gurugeya.urls'
+ROOT_URLCONF = 'sinhalaforkids.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'gurugeya.wsgi.application'
+WSGI_APPLICATION = 'sinhalaforkids.wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates')
