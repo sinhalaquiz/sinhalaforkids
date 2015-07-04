02286100cf1de573139c38767b474c55f22bb08c
Dushara Jayasinghe <nidujay@gmail.com>
2015-07-05 08:39:28 +1000

    Added management commands to import data
    The two commands import media and sinhala words. The import is
    from CSV files. The columns should match the object field names.
    There can't be any extra columns.
    
    Importing words also generates links to existing media objects.
    This means maximum conveniance is gained by importing all the media
    first followed by the import of words.

---
+++ wordquiz/management/commands/importmedia.py
@@ -0,0 +1,24 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+from wordquiz.models import MediaObject
+
+def save_data(data):
+    mo = MediaObject(**data)
+    mo.save()
+
+def save_from_file(filename):
+    with open(filename, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            save_data(row)
+
+    return None
+
+class Command(BaseCommand):
+    args = '<csvfile>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        for csvfile in args:
+            self.stdout.write('importing media objects from %s\n' % (csvfile))
+            save_from_file(csvfile)
---
+++ wordquiz/management/commands/importwords.py
@@ -0,0 +1,29 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+from wordquiz.models import SinhalaWord, WordCategory, MediaObject
+
+def save_data(data):
+    wc = WordCategory.objects.get(category=data['category'])
+    data['category'] = wc
+    w = SinhalaWord(**data)
+    w.save()
+    mo = MediaObject.objects.filter(short_name=data['english'])
+    w.media = mo
+    w.save()
+
+def save_from_file(filename):
+    with open(filename, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            save_data(row)
+
+    return None
+
+class Command(BaseCommand):
+    args = '<csvfile>'
+    help = 'Imports list of sinhala words linking available medi'
+
+    def handle(self, *args, **options):
+        for csvfile in args:
+            self.stdout.write('importing words from %s\n' % (csvfile))
+            save_from_file(csvfile)
