a49fb6456ff6934debe9bb4555e5373e90f41c71
Dushara Jayasinghe <nidujay@gmail.com>
2015-08-04 19:53:54 +1000

    Added home page
    The home page lists all the apps

--- sinhalaforkids/urls.py
+++ sinhalaforkids/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+class HomePageView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['apps'] = ['choosepic', 'wordquiz']
+        return context
+
 urlpatterns = patterns('',
-    url(r'^wordquiz/', include('wordquiz.urls')),
-    url(r'^choosepic/', include('choosepic.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+        url(r'^$', HomePageView.as_view()),
+        url(r'^wordquiz/', include('wordquiz.urls')),
+        url(r'^choosepic/', include('choosepic.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        )
