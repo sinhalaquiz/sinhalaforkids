395bf71f7c2cff1b6f0c436c54abca11e228e4ce
Dushara Jayasinghe <nidujay@gmail.com>
2015-10-24 08:19:45 +1100

    WIP Decoupling apps with site HTML
    * Re-introduced the activities for words and sentences in
      relavent index pages
    * Created a new app HTML template to allow embedding app inside
      site HTML. This way app specific HTML can be totally independent
      of the site while still retaining the site appearance.

--- sinhalaforkids/urls.py
+++ sinhalaforkids/urls.py
@@ -37,34 +37,72 @@ class HomePageView(TemplateView):
         return context
 
 class AlphabetPageView(TemplateView):
-    template_name="index.html"
+    template_name="app.html"
 
     def get_context_data(self, **kwargs):
         context = super(AlphabetPageView, self).get_context_data(**kwargs)
-        context['title'] = 'Lessons for the Sinhala alphabet'
+        context['title'] = 'Sinhala alphabet'
         context['desc'] = 'Learn the recognise the shapes and sounds of the ' \
             'Sinhala alphabet'
         context['heading'] = 'Choose your lesson'
+        context['url'] = 'http://validator.w3.org/'
+
+        return context
+
+class WordsPageView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(WordsPageView, self).get_context_data(**kwargs)
+        context['title'] = 'Learn Sinhala words'
+        context['desc'] = 'Expand your vocabulary through these activities'
+        context['heading'] = 'Choose your activity'
         context['apps'] = [
             { 
-                'url' : 'alphalesson/{0}'.format(i),
-                'name' : 'Lesson {0}'.format(i),     
-                'img' : 'alphabet-{0}.png'.format(i), 
-            } for i in range(1, 11)
+                'url' : 'wordquiz',
+                'name' : 'Word Quiz',     
+                'img' : 'wordquiz.png', 
+                'desc' : 'Learn the meaning and sound of words through a quiz' 
+                },
+            {
+                'url' : 'choosepic',
+                'name' : 'Pick the pic',  
+                'img' : 'choosepic.png', 
+                'desc' : 'Choose the picture that matches a given word' 
+            },
         ]
 
         return context
 
+class SentencesPageView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SentencesPageView, self).get_context_data(**kwargs)
+        context['title'] = 'Learning Sentences'
+        context['desc'] = 'Activities to help learn to read and understand sentences in Sinhala'
+        context['heading'] = 'Choose your activity'
+        context['apps'] = [
+            { 
+                'url' : 'treasure',
+                'name' : 'Treasure Hunt', 
+                'img' : 'treasure.png', 
+                'desc' : 'Follow the picture trail using the given clues' 
+                },
+            ]
+
+        return context
+
 urlpatterns = patterns('',
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    url(r'^$', HomePageView.as_view()),
-    url(r'^alphabet', AlphabetPageView.as_view()),
-    url(r'^about', TemplateView.as_view(template_name='about.html')),
-    url(r'^contact', TemplateView.as_view(template_name='contact.html')),
-    url(r'^words', TemplateView.as_view(template_name='words.html')),
-    url(r'^sentences', TemplateView.as_view(template_name='sentences.html')),
-    url(r'^wordquiz/', include('wordquiz.urls')),
-    url(r'^choosepic/', include('choosepic.urls')),
-    url(r'^treasure/', include('treasure.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+        url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+        url(r'^$', HomePageView.as_view()),
+        url(r'^alphabet', AlphabetPageView.as_view()),
+        url(r'^words', WordsPageView.as_view()),
+        url(r'^sentences', SentencesPageView.as_view()),
+        url(r'^about', TemplateView.as_view(template_name='about.html')),
+        url(r'^contact', TemplateView.as_view(template_name='contact.html')),
+        url(r'^wordquiz/', include('wordquiz.urls')),
+        url(r'^choosepic/', include('choosepic.urls')),
+        url(r'^treasure/', include('treasure.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        )
