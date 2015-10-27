a0957bae053cab1182fec87f38186f24f17578c8
Dushara Jayasinghe <nidujay@gmail.com>
2015-10-28 08:06:12 +1100

    alphabet lesson index populated from database

--- alphabet/views.py
+++ alphabet/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render_to_response
 from alphabet.models import Lesson
+from collections import defaultdict
+
+def get_lessons():
+    d = defaultdict(list)
+    for o in Lesson.objects.all():
+        d[o.number].append(o.letter)
+    # Getting rid of the default_factory allows us to use it
+    # in a template. Otherwise we have to copy it to a dict
+    x = { str(i[0]):i[1] for i in d.items()}
+    return sorted(x.iteritems())
 
 def index(request):
-    context = {'lessons' : ['a t', 'g m']}
+    context = {'lessons' : get_lessons()}
     return render_to_response('alphabet/index.html', context)
 
 def lesson(request, id=0):
